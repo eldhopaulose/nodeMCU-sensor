478d831d8f4658745b77bbe9495a03cea7427249
Eldho Paulose <50209592+eldhopaulose@users.noreply.github.com>
2023-03-12 02:53:50 +0530

    Update index.js
    edited last

--- index.js
+++ index.js
@@ -1,92 +1,54 @@
-const mongoose = require('mongoose');
 const express = require('express');
-
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = normalizePort(process.env.PORT || '7001');
-app.set('port', port);
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+const port = process.env.PORT || 3000;
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+// Connection URL and database name
+const url = 'mongodb+srv://eldhopaulose0485:xyzel_025@cluster0.4sjqm.mongodb.net/NodeMcu?retryWrites=true&w=majority';
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+// Set up default mongoose connection
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected successfully to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-  return false;
-}
-// Connection URI and options
-const uri = 'mongodb+srv://eldhopaulose0485:xyzel_025@cluster0.4sjqm.mongodb.net/NodeMcu?retryWrites=true&w=majority';
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-
-// Define a schema for the sensor data
+// Define schema for sensor data
 const SensorSchema = new mongoose.Schema({
-    temperature: Number,
-    humidity: Number,
-    soil_moisture: Number,
-    ph: Number,
-    
-  });
+  temperature: Number,
+  humidity: Number,
+  soil_moisture: Number,
+  ph: Number,
+});
 
-// Create a model based on the schema
+// Create model from schema
 const Sensor = mongoose.model('Sensor', SensorSchema);
 
+// Use body-parser middleware to parse request body
+app.use(bodyParser.json());
 
+// Set up a route to handle POST requests from the sensor
+app.post('/api/data', async (req, res) => {
+  try {
+    // Find and delete the previous document
+    const previousData = await Sensor.findOneAndDelete();
+    console.log('Previous data deleted:', previousData);
 
-// Connect to MongoDB Atlas
-mongoose.connect(uri, options)
-  .then(() => {
-    console.log('Connected successfully to MongoDB Atlas');
-  })
-  .catch((err) => {
-    console.log('Error connecting to MongoDB Atlas:', err);
-  });
+    // Create new document from request body
+    const sensorData = new Sensor(req.body);
 
-// Define a route that fetches the sensor data
-app.get('/api/data', async (req, res) => {
-    try {
-      // Find all documents in the collection
-      const sensorData = await Sensor.find().exec();
-  
-      console.log('Found the following documents:');
-      console.log(sensorData);
-  
-      res.json(sensorData);
-    } catch (err) {
-      console.error(err);
-      res.sendStatus(500);
-    }
-  });
+    // Save document to database
+    const savedData = await sensorData.save();
 
-  app.post('/api/data', async (req, res) => {
-    try {
-      // Find and delete the previous document
-      const previousData = await Sensor.findOneAndDelete();
-      console.log('Previous data deleted:', previousData);
-  
-      // Create new document from request body
-      const sensorData = new Sensor(req.body);
-  
-      // Save document to database
-      const savedData = await sensorData.save();
-  
-      console.log('New data inserted:', savedData);
-      res.sendStatus(200);
-    } catch (err) {
-      console.error(err);
-      res.sendStatus(500);
-    }
-  });
+    console.log('New data inserted:', savedData);
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
 
+// Start server
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
