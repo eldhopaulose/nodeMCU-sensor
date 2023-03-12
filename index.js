aa4a1ec37368f483790ca628469180dd8204dc95
Eldho Paulose <50209592+eldhopaulose@users.noreply.github.com>
2023-03-12 14:11:30 +0530

    Update index.js
    chatGpt

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-
-// Use environment variables provided by Render.com
-const port = process.env.PORT || 3000;
-const ip = process.env.IP || '0.0.0.0';
+const port = process.env.PORT || 3000; // Use environment variable or default port
+const ip = process.env.IP || 'localhost'; // Use environment variable or default IP address
 
 // Connection URL and database name
-const url = 'mongodb+srv://eldhopaulose0485:xyzel_025@cluster0.4sjqm.mongodb.net/NodeMcu?retryWrites=true&w=majority';
+const url = 'mongodb+srv://eldhopaulose0485:xyzel_025@cluster0.4sjqm.mongodb.net/NodeMcu?retryWrites=true&w=majority'; // Use environment variable or default URL
 
 // Set up default mongoose connection
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,7 +21,7 @@ const db = mongoose.connection;
 // Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function() {
-  console.log("Connected successfully to MongoDB Atlas");
+  console.log("Connected successfully to MongoDB");
 });
 
 // Define schema for sensor data
@@ -28,65 +29,35 @@ const SensorSchema = new mongoose.Schema({
   temperature: Number,
   humidity: Number,
   soil_moisture: Number,
-  ph: Number,  
+  ph: Number,
+  timestamp: { type: Date, default: Date.now }
 });
 
 // Create model from schema
 const Sensor = mongoose.model('Sensor', SensorSchema);
 
 // Use body-parser middleware to parse request body
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Connect to MongoDB Atlas
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected successfully to MongoDB Atlas');
-  })
-  .catch((err) => {
-    console.log('Error connecting to MongoDB Atlas:', err);
-  });
-
-// Define a route that fetches the sensor data
-app.get('/api/data', async (req, res) => {
-    try {
-      // Find all documents in the collection
-      const sensorData = await Sensor.find().exec();
-  
-      console.log('Found the following documents:');
-      console.log(sensorData);
-  
-      res.json(sensorData);
-    } catch (err) {
-      console.error(err);
-      res.sendStatus(500);
-    }
-});
 
 // Set up a route to handle POST requests from the sensor
 app.post('/api/data', async (req, res) => {
-    try {
-      // Find and delete the previous document
-      const previousData = await Sensor.findOneAndDelete();
-      console.log('Previous data deleted:', previousData);
-  
-      // Create new document from request body
-      const sensorData = new Sensor({
-        temperature: req.body.temperature,
-        humidity: req.body.humidity,
-        soil_moisture: req.body.soil_moisture,
-        ph: req.body.ph
-      });
-  
-      // Save document to database
-      const savedData = await sensorData.save();
-  
-      console.log('New data inserted:', savedData);
-      res.sendStatus(200);
-    } catch (err) {
-      console.error(err);
-      res.sendStatus(500);
-    }
+  try {
+    // Find and delete the previous document
+    const previousData = await Sensor.findOneAndDelete();
+    console.log('Previous data deleted:', previousData);
+
+    // Create new document from request body
+    const sensorData = new Sensor(req.body);
+
+    // Save document to database
+    const savedData = await sensorData.save();
+
+    console.log('New data inserted:', savedData);
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 });
 
 // Start server on specific IP address and port
